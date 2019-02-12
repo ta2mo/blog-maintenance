a24228c22c69df5c84f0cfd7cd1adb608524c2a0
ta2mo <naoto.tanimoto@gmail.com>
2019-02-12 14:04:01 +0900

    fix meta for google site verification

--- nuxt-template/nuxt.config.js
+++ nuxt-template/nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: '{{escape description }}' },
+      { name: 'google-site-verification', content:'phkCSJT4fL-KjD1tKs9WEBMJIazwJL8cJYGY7FRGVE0' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
